f40bef1062ea5b54b68df81631ac0d83491b6245
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-02-06 08:16:33 +0100

    Add WithIcon protocol

---
+++ Sources/MonkiProjectsModel/Utilities/WithIcon.swift
@@ -0,0 +1,55 @@
+//
+//  WithIcon.swift
+//  MonkiProjectsModel
+//
+//  Created by Rémi Bardon on 01/10/2020.
+//  Copyright © 2020 Monki Projects. All rights reserved.
+//
+
+// MARK: - Protocol
+
+public protocol WithIcon {
+	
+	var iconName: String? { get }
+	
+}
+
+// MARK: - UIKit
+
+#if canImport(UIKit)
+import UIKit
+
+extension WithIcon {
+	
+	/// Icons from [Icons8](https://icons8.com)
+	@available(iOS 13.0, *)
+	public var uiIcon: UIImage? {
+		if let iconName = self.iconName {
+			return UIImage(named: iconName, in: .fixedModule, compatibleWith: nil)
+		} else {
+			return nil
+		}
+	}
+	
+}
+#endif
+
+// MARK: - SwiftUI
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+extension WithIcon {
+	
+	/// Icons from [Icons8](https://icons8.com)
+	@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+	public var icon: Image? {
+		if let iconName = self.iconName {
+			return Image(iconName, bundle: .fixedModule)
+		} else {
+			return nil
+		}
+	}
+	
+}
+#endif
