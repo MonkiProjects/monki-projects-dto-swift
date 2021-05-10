8d47e9dc3ec8a44a728bee8fb25c6062526159de
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-05-10 08:16:34 +0200

    Add `User.Token.Private`

---
+++ Sources/MonkiProjectsModel/Users/UserToken+Private.swift
@@ -0,0 +1,31 @@
+//
+//  UserToken+Private.swift
+//  MonkiProjectsModel
+//
+//  Created by Rémi Bardon on 08/06/2020.
+//  Copyright © 2020 Monki Projects. All rights reserved.
+//
+
+import Foundation
+
+extension User {
+	
+	public enum Token {
+		
+		public struct Private: Codable, Hashable {
+			
+			public let value: String
+			public let expiresAt: Date?
+			public let createdAt: Date
+			
+			public init(value: String, expiresAt: Date?, createdAt: Date) {
+				self.value = value
+				self.expiresAt = expiresAt
+				self.createdAt = createdAt
+			}
+			
+		}
+		
+	}
+	
+}
