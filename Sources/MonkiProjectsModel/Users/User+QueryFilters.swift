3f7f76ec66c267c14aa07df853cb4342ee0301e7
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-05-20 18:41:50 +0200

    Add `User.QueryFilters`

---
+++ Sources/MonkiProjectsModel/Users/User+QueryFilters.swift
@@ -0,0 +1,27 @@
+//
+//  User+QueryFilters.swift
+//  MonkiProjectsModel
+//
+//  Created by Rémi Bardon on 20/05/2021.
+//  Copyright © 2021 Monki Projects. All rights reserved.
+//
+
+import Foundation
+
+extension User {
+	
+	public struct QueryFilters: Codable, Hashable {
+		
+		public var username, displayName: String?
+		
+		public init(
+			username: String? = nil,
+			displayName: String? = nil
+		) {
+			self.username = username
+			self.displayName = displayName
+		}
+		
+	}
+	
+}
