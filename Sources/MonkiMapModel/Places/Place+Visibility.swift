27bb60c2d88fe5675d4a389085e639220de50742
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-11-10 22:30:08 +0100

    ✨ Put back `Place.State` bu rename it to `Place.Visibility` and remove some cases
    - Also put it in `Place.Metadata` instead of `Place.Public`

---
+++ Sources/MonkiMapModel/Places/Place+Visibility.swift
@@ -0,0 +1,30 @@
+//
+//  Place+Visibility.swift
+//  MonkiMapModel
+//
+//  Created by Rémi Bardon on 24/06/2020.
+//  Copyright © 2020 Monki Projects. All rights reserved.
+//
+
+import Foundation
+import MonkiProjectsModel
+
+extension Place {
+	
+	// The visibility of a place.
+	public enum Visibility: String, Hashable, Codable, CaseIterable, DefaultCaseDecodable {
+		
+		public static var defaultCase: Self = .unknown
+		
+		// Special case reserved for backwards compatibility.
+		case unknown
+		
+		// State of a place stored online but not visible to anyone except its creator.
+		case `private`
+		
+		// State of a place visible by anyone.
+		case `public`
+		
+	}
+	
+}
