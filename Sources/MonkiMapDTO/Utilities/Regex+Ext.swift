deb8d1184b85f2f15bcb94ba9bcb14be32537801
Rémi Bardon <remi.bardon.dev@gmail.com>
2022-01-17 15:04:58 +0100

    💡 Update comments

--- Sources/MonkiMapDTO/Utilities/Regex+Ext.swift
+++ Sources/MonkiMapDTO/Utilities/Regex+Ext.swift
@@ -3,7 +3,7 @@
 //  MonkiMapDTO
 //
 //  Created by Rémi Bardon on 05/07/2019.
-//  Copyright © 2020 Monki Projects. All rights reserved.
+//  Copyright © 2019 Monki Projects. All rights reserved.
 //
 
 import Foundation
@@ -12,41 +12,43 @@ extension NSRegularExpression {
 	
 	/// Tests if a `String` matches the regular expression.
 	///
-	/// - Parameters:
-	///   - string: A `String` to test against the regular expression.
-	/// - Returns: `true` if `string` matched regular expression, `false` otherwise.
+	/// Usage:
 	///
-	/// # Notes: #
-	/// - Comes from [How to use regular expressions in Swift](https://www.hackingwithswift.com/articles/108/how-to-use-regular-expressions-in-swift)
-	///
-	/// # Example #
 	/// ```
 	/// let regex = NSRegularExpression("[a-z]at")
 	/// regex.matches("hat")
 	/// ```
+	///
+	/// - Parameters:
+	///   - string: A `String` to test against the regular expression.
+	/// - Returns: `true` if `string` matched regular expression, `false` otherwise.
+	/// - Note: Comes from [How to use regular expressions in Swift](https://www.hackingwithswift.com/articles/108/how-to-use-regular-expressions-in-swift)
 	func matches(_ string: String) -> Bool {
+		return self.firstMatch(in: string) != nil
+	}
+	
+	func firstMatch(in string: String) -> NSTextCheckingResult? {
 		let range = NSRange(location: 0, length: string.utf16.count)
-		return firstMatch(in: string, options: [], range: range) != nil
+		return self.firstMatch(in: string, range: range)
 	}
 	
 }
 
 extension String {
 	
+	/// Overload Swift’s contains operator to work with regular expressions.
 	///
+	/// Usage:
+	///
+	/// ```
+	/// "hat" ~= "[a-z]at"
+	/// ```
 	///
 	/// - Parameters:
 	///   - lhs: Left hand side `String`.
 	///   - rhs: Right hand side `String`.
 	/// - Returns: `true` if `lhs` matched `rhs` regular expression, `false` otherwise.
-	///
-	/// # Notes: #
-	/// - Comes from [How to use regular expressions in Swift](https://www.hackingwithswift.com/articles/108/how-to-use-regular-expressions-in-swift)
-	///
-	/// # Example #
-	/// ```
-	/// "hat" ~= "[a-z]at"
-	/// ```
+	/// - Notes: Comes from [How to use regular expressions in Swift](https://www.hackingwithswift.com/articles/108/how-to-use-regular-expressions-in-swift)
 	static func ~= (lhs: String, rhs: String) -> Bool {
 		guard let regex = try? NSRegularExpression(pattern: rhs) else { return false }
 		let range = NSRange(location: 0, length: lhs.utf16.count)
